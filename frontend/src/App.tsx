2010cc2822555cd2f3adc473ee585ba44a754095
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-06-19 10:21:52 +0200

    create and render sampleProducts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-
-import './App.css'
+import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world</h1>
-        <div className='bg-gray-800 mt-20'>re</div>
+      <h2 className="text-3xl font-bold underline text-center mt-8">Products</h2>
+      <div className="flex gap-4 justify-around items-center p-4 m-4">
+        {sampleProducts.map((product) => (
+          <div key={product.slug} className="flex flex-col gap-2 justify-center items-center p-4 m-4">
+            <p className="text-center">{product.name}</p>
+            <img src={product.image} alt={product.name} />
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
