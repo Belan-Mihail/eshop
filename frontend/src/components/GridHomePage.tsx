e6ccafeb947f73582fc1b2f352a5e91fb4a3bf23
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-11 09:35:15 +0200

    setting up the exit animation of the first image in the grid template 1

--- frontend/src/components/GridHomePage.tsx
+++ frontend/src/components/GridHomePage.tsx
@@ -9,88 +9,102 @@ export type Props = {
 const GridHomePage = ({ selectedProductsForHomePage, gridNumber }: Props) => {
   const [product1, product2, product3, product4, product5, product6] =
     selectedProductsForHomePage;
+
+
   return (
     <>
-      {gridNumber === 1 && (
-        <AnimatePresence>
-          <motion.div
-            className="grid grid-cols-3 grid-rows-4 gap-3 h-screen mx-8"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, transition: { duration: 2 } }}
-            transition={{ duration: 2 }}
-          >
+      <AnimatePresence>
+      {gridNumber === 1  && (
+        <motion.div
+          className="grid grid-cols-3 grid-rows-4 gap-3 h-screen mx-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2.5 }}
+        >
+          
+            {gridNumber === 1 && (
             <motion.div
               className="col-span-2  overflow-hidden"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              exit={{ opacity: 0, left: -10, transition: { duration: 1 } }}
-              transition={{ delay: 1.5 }}
+              exit={{ opacity: 0, x: '-400px' }}
+              transition={{duration: 2}}
+
             >
               <img
                 className="object-cover max-w-[100%] h-[100%] w-[100%]"
                 src={product1.image}
                 alt={product1.name}
+                
               />
             </motion.div>
-            <div className="row-span-2 col-start-2 row-start-2 overflow-hidden">
-              {" "}
-              <img
-                className="object-cover max-w-[100%] h-[100%] w-[100%]"
-                src={product2.image}
-                alt={product2.name}
-              />
-            </div>
-            <div className="col-start-3 row-start-1  overflow-hidden">
-              {" "}
-              <img
-                className="object-cover max-w-[100%] h-[100%] w-[100%]"
-                src={product3.image}
-                alt={product3.name}
-              />
-            </div>
-            <div className="row-span-3 col-start-1 row-start-2  overflow-hidden">
-              {" "}
-              <img
-                className="object-cover max-w-[100%] h-[100%] w-[100%]"
-                src={product4.image}
-                alt={product4.name}
-              />
-            </div>
-            <div className="row-span-3 row-start-2  overflow-hidden">
-              {" "}
-              <img
-                className="object-cover max-w-[100%] h-[100%] w-[100%]"
-                src={product5.image}
-                alt={product5.name}
-              />
-            </div>
-            <div className="col-start-2 row-start-4  overflow-hidden">
-              <img
-                className="object-cover max-w-[100%] h-[100%] w-[100%]"
-                src={product6.image}
-                alt={product6.name}
-              />
-            </div>
-          </motion.div>
-        </AnimatePresence>
-      )}
-
-      {gridNumber === 2 && (
-        <motion.div
-          className="grid grid-cols-3 grid-rows-4 gap-4 h-screen mx-8"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1, delay: 1 }}
-        >
-          <div className="col-start-2 row-start-1">
+            )}
+          
+          <div className="row-span-2 col-start-2 row-start-2 overflow-hidden">
+            {" "}
+            <img
+              className="object-cover max-w-[100%] h-[100%] w-[100%]"
+              src={product2.image}
+              alt={product2.name}
+            />
+          </div>
+          <div className="col-start-3 row-start-1  overflow-hidden">
+            {" "}
+            <img
+              className="object-cover max-w-[100%] h-[100%] w-[100%]"
+              src={product3.image}
+              alt={product3.name}
+            />
+          </div>
+          <div className="row-span-3 col-start-1 row-start-2  overflow-hidden">
+            {" "}
+            <img
+              className="object-cover max-w-[100%] h-[100%] w-[100%]"
+              src={product4.image}
+              alt={product4.name}
+            />
+          </div>
+          <div className="row-span-3 row-start-2  overflow-hidden">
+            {" "}
+            <img
+              className="object-cover max-w-[100%] h-[100%] w-[100%]"
+              src={product5.image}
+              alt={product5.name}
+            />
+          </div>
+          <div className="col-start-2 row-start-4  overflow-hidden">
             <img
               className="object-cover max-w-[100%] h-[100%] w-[100%]"
               src={product6.image}
               alt={product6.name}
             />
           </div>
+        </motion.div>
+      )}
+      
+
+      {gridNumber === 3 && (
+        <motion.div
+          className="grid grid-cols-3 grid-rows-4 gap-4 h-screen mx-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, transition: { duration: 2 } }}
+          transition={{ duration: 2 }}
+        >
+          <motion.div
+            className="col-start-2 row-start-1"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { duration: 2 } }}
+            transition={{ duration: 2 }}
+          >
+            <img
+              className="object-cover max-w-[100%] h-[100%] w-[100%]"
+              src={product6.image}
+              alt={product6.name}
+            />
+          </motion.div>
           <div className="row-span-3 col-start-2 row-start-2">
             <img
               className="object-cover max-w-[100%] h-[100%] w-[100%]"
@@ -232,6 +246,7 @@ const GridHomePage = ({ selectedProductsForHomePage, gridNumber }: Props) => {
           </div>
         </motion.div>
       )}
+      </AnimatePresence>
     </>
   );
 };
